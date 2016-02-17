5c240fc4c5dfde6b752f9860da4a2035bedbd478
Daniel Medero Garcia <danimg.102@gmail.com>
2016-02-17 12:10:46 -0500

    adding default route to systems

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'systems#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
